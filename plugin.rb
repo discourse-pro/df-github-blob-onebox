ec560df6c055a57494b3c9cf401d06ac7d8a7add
Dmitry Fedyuk <dfediuk@gmail.com>
2018-01-24 05:42:07 +0600

    no message

--- plugin.rb
+++ plugin.rb
@@ -111,12 +111,19 @@ class Onebox::Engine::GithubBlobOnebox
 
 	  @file = m[:file]
 	  @lang = Onebox::FileTypeFinder.from_file_name(m[:file])
-	  #contents = open("https://raw.githubusercontent.com/#{m[:user]}/#{m[:repo]}/#{m[:sha1]}/#{m[:file]}", read_timeout: timeout).read
-	  # 2018-01-22
+	  # 2018-01-23
+	  # 1) https://developer.github.com/changes/2014-04-25-user-content-security
+	  # 2) «`raw.github.com` does not redirect to `raw.githubusercontent.com` for me»:
+	  # https://github.com/processing/processing-docs/issues/532
+	  # contents = open("https://raw.githubusercontent.com/#{m[:user]}/#{m[:repo]}/#{m[:sha1]}/#{m[:file]}", read_timeout: timeout).read
+	  # 2018-01-23
 	  # «No traffic limits or throttling»
 	  # https://rawgit.com
 	  # https://github.com/rgrove/rawgit
-	  contents = open("https://cdn.rawgit.com/#{m[:user]}/#{m[:repo]}/#{m[:sha1]}/#{m[:file]}", read_timeout: timeout).read
+	  io = open("https://cdn.rawgit.com/#{m[:user]}/#{m[:repo]}/#{m[:sha1]}/#{m[:file]}", read_timeout: timeout)
+	  contents = io.read
+	  # 2018-01-24 https://stackoverflow.com/a/4795782
+	  io.close
 	  #sleep 1 # 2018-01-22
 
 	  contents_lines = contents.lines           #get contents lines
