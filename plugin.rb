8c88672388d66d10d4c2a7b4b9926380e1176ac5
Dmitry Fedyuk <dfediuk@gmail.com>
2018-01-24 08:12:19 +0600

    no message

--- plugin.rb
+++ plugin.rb
@@ -3,9 +3,28 @@
 # version: 1.0.0
 # authors: Dmitry Fedyuk
 # url: https://discourse.pro
+Rake::Task.define_task 'df:refresh_oneboxes' => :environment do
+	require 'post'
+	require 'rake/task'
+	require 'site_setting_extension'
+	puts "Rebaking post markdown for '#{RailsMultisite::ConnectionManagement.current_db}'"
+	disable_edit_notifications = SiteSetting.disable_edit_notifications
+	SiteSetting.disable_edit_notifications = true
+	total = Post.count
+	count = 0
+	Post.find_each do |post|
+		count += 1
+		post.df_delay_for = (5 * count).seconds
+		post.rebake!(invalidate_oneboxes: true)
+		print "\r%9d / %d (%5.1f%%)" % [count, total, ((count.to_f / total.to_f) * 100).round(1)]
+	end
+	SiteSetting.disable_edit_notifications = disable_edit_notifications
+	puts "", "#{count} posts done!", "-" * 50
+end
 after_initialize do
 	require 'post'
 	Post.module_eval do
+		attr_accessor :df_delay_for
 		# Enqueue post processing for this post
 		def trigger_post_process(bypass_bump = false)
 			args = {
@@ -16,6 +35,9 @@ after_initialize do
 			args[:invalidate_oneboxes] = true if invalidate_oneboxes.present?
 			args[:cooking_options] = self.cooking_options
 			# 2018-01-23
+			if @df_delay_for
+				args[:delay_for] = @df_delay_for								
+			end
 			#args[:delay_for] = 5.seconds
 			Jobs.enqueue(:process_post, args)
 			DiscourseEvent.trigger(:after_trigger_post_process, self)
