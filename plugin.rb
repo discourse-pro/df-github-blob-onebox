9adb57023f6b0350ba4630736d934689246c10c1
Dmitry Fedyuk <dfediuk@gmail.com>
2018-01-24 08:26:49 +0600

    no message

--- plugin.rb
+++ plugin.rb
@@ -3,10 +3,12 @@
 # version: 1.0.0
 # authors: Dmitry Fedyuk
 # url: https://discourse.pro
-Rake::Task.define_task 'df:refresh_oneboxes' => :environment do
+Rake::Task.define_task 'df:refresh_oneboxes', [:delay] => :environment do |_, args|
 	require 'post'
 	require 'rake/task'
 	require 'site_setting_extension'
+	args.with_defaults(delay: 5)
+	delay = args[:delay]&.to_i
 	puts "Rebaking post markdown for '#{RailsMultisite::ConnectionManagement.current_db}'"
 	disable_edit_notifications = SiteSetting.disable_edit_notifications
 	SiteSetting.disable_edit_notifications = true
@@ -14,7 +16,7 @@ Rake::Task.define_task 'df:refresh_oneboxes' => :environment do
 	count = 0
 	Post.find_each do |post|
 		count += 1
-		post.df_delay_for = (5 * count).seconds
+		post.df_delay_for = (delay * count).seconds
 		post.rebake!(invalidate_oneboxes: true)
 		print "\r%9d / %d (%5.1f%%)" % [count, total, ((count.to_f / total.to_f) * 100).round(1)]
 	end
